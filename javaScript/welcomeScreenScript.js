10d761a49c1dc1ed5f2409aa97def9428fdfc5b3
Hywie Martin <HywieMartin@hotmail.com>
2015-11-13 18:57:56 +0000

    Added minimise button to welcome screen.

--- javaScript/welcomeScreenScript.js
+++ javaScript/welcomeScreenScript.js
@@ -2,6 +2,7 @@
 
 window.onload = function(){
   document.getElementById("exitAppElement").onclick = function() { window.close(); };
+  document.getElementById("minAppElement").onclick = function() { chrome.app.window.current().minimize(); };
 };
 
 
